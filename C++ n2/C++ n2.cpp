c12b93b416209a9ff5617e43df582400d8027e27
HP <HP@DESKTOP-NMBILJD>
2024-11-02 18:07:28 +0100

    Add project files.

---
+++ C++ n2/C++ n2.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string myname = "mohammade";
+	int myage = 44; "years";
+	string mycity = "marrakech";
+	string mycountry = "morroco";
+	double salary = 50000;
+	double msalary = 60000;
+	char mygender = 'm';
+	bool mrarried = true;
+	 
+	cout << "myname: " << myname << endl;
+	cout << "myage: " << myage << endl;
+	cout << " mycity: " << mycity << endl;
+	cout << " mycountry: " << mycountry << endl;
+	cout << "mysalary: " << salary << endl;
+	cout << "msalary: " << msalary << endl;
+	cout << "mygender: " << mygender << endl;
+	cout << " mrarried: " << mrarried << endl;
+
+
+
+
+
+	return 0;
+}
